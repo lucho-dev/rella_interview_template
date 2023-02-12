601a8c33f8d0ec9e7706f0a30d814a3eb78b9129
Luis Alberto Carrettoni <lcarrettoni@hotmail.com>
2023-02-12 13:24:10 -0300

    partial coding

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:interview_template/my_object.dart';
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
+class MyHomePage
+    extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomePage> createState() => _MyHopePageState();
+}
+
+class _MyHopePageState extends State<MyHomePage> {
   final catObject = CatObject.create();
 
   @override
@@ -12,7 +18,56 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cat-alogue'),
       ),
-      body: Container(),
+      body: _body(),
     );
+  }
+
+  Widget _body() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: _cells(),
+      ),
+    );
+  }
+
+  List<Widget> _cells() {
+    List<Widget> cells = [];
+
+    const TextStyle titleStyle = TextStyle(fontSize: 20, color: Colors.black);
+    const TextStyle subTitle = TextStyle(fontSize: 18, color: Colors.black);
+    const TextStyle likeStyle = TextStyle(fontSize: 16, color: Colors.black);
+
+    cells.add(
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: InkWell(
+          onTap: () => _onTapOn(catObject),
+          child: Card(
+            child:
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(catObject.title, style: titleStyle,),
+                  const SizedBox(height: 4,),
+                  Text(catObject.subtitle, style: subTitle,),
+                  Image.network(catObject.imageUrl, fit: BoxFit.fitHeight, height: 200,),
+                  Text("Likes: ${catObject.likes}", style: likeStyle,),
+                ],
+              ),
+            )
+          ),
+        ),
+      )
+    );
+
+    return cells;
+  }
+
+  _onTapOn(CatObject catObject) {
+    setState(() {
+      catObject.likes++;
+    });
   }
 }
